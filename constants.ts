602a63205100cfe29d5a430ec3e05167b74f3d54
Saidur Rahman <mdsaidul2k20@gmail.com>
2026-02-02 00:27:00 +0900

    Create constants.ts

---
+++ constants.ts
@@ -0,0 +1,4 @@
+export const NIGHT_SHIFT_START = 22; // 22:00
+export const NIGHT_SHIFT_END = 5;    // 05:00
+export const DAILY_LIMIT = 8;        // 8 hours
+export const WEEKLY_STUDENT_LIMIT = 28; // 28 hours for students
